ea0ec4e9fba45dae6b411ecc8aa1c7aeae62f4c8
Rohnak Agarwal <rrka79wal@gmail.com>
2021-04-01 21:30:46 +0530

    clock

--- 15_clock/clock.go
+++ 15_clock/clock.go
@@ -2,76 +2,30 @@ package clock
 
 import "fmt"
 
+const minutesInADay = 24 * 60
+
 type Clock struct {
 	h, m int
 }
 
-func (c Clock) Add(m int) Clock {
-	c.m += m
-	c.h += c.m / 60
-	c.m %= 60
-	if c.h >= 24 || c.m >= 60 {
-		c.h += c.m / 60
-		c.h %= 24
-		c.m %= 60
-	}
-	return Clock{c.h, c.m}
+func New(h, m int) Clock {
+	minutes := ((60*h+m)%minutesInADay + minutesInADay) % minutesInADay
+
+	return Clock{minutes / 60, minutes % 60}
 }
 
-func (c Clock) Subtract(m int) Clock {
-	c.m -= m
-	c.h += c.m / 60
-	c.m %= 60
-	if c.m < 0 {
-		c.m %= 60
-		c.m = 60 + c.m
-		c.h--
-	}
-	if c.h < 0 {
-		c.h %= 24
-		c.h = 24 + c.h
-	}
-	if c.h >= 24 || c.m >= 60 {
-		c.h += c.m / 60
-		c.h %= 24
-		c.m %= 60
-	}
-	return Clock{c.h, c.m}
+func (c Clock) Add(min int) Clock {
+	minutes := ((60*c.h+c.m+min)%minutesInADay + minutesInADay) % minutesInADay
+
+	return Clock{minutes / 60, minutes % 60}
+}
+
+func (c Clock) Subtract(min int) Clock {
+	minutes := ((60*c.h+c.m-min)%minutesInADay + minutesInADay) % minutesInADay
+
+	return Clock{minutes / 60, minutes % 60}
 }
 
 func (c Clock) String() string {
-	s := ""
-	if c.h < 10 {
-		s = s + "0"
-	}
-	s = s + fmt.Sprint(c.h)
-	s = s + ":"
-	if c.m < 10 {
-		s = s + "0"
-	}
-	s = s + fmt.Sprint(c.m)
-	return s
-}
-
-func New(h, m int) Clock {
-	if h < 0 || m < 0 {
-		if m < 0 {
-			h += m / 60
-			m %= 60
-		}
-		if h < 0 {
-			h %= 24
-			h = 24 + h
-		}
-		if m < 0 {
-			h--
-			m = 60 + m
-		}
-	}
-	if h >= 24 || m >= 60 {
-		h += m / 60
-		h %= 24
-		m %= 60
-	}
-	return Clock{h, m}
+	return fmt.Sprintf("%02d:%02d", c.h, c.m)
 }
