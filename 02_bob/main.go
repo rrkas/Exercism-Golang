a1aa536eb6f84ea7b79d99a690085fc294226a1b
Rohnak Agarwal <rrka79wal@gmail.com>
2021-04-01 09:51:55 +0530

    Bob

---
+++ 02_bob/main.go
@@ -0,0 +1,48 @@
+// The bob package handles forming responses for Alice
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+// Hey takes in a remark and returns a response to the remark as a string
+func Hey(remark string) string {
+	remark = strings.TrimSpace(remark)
+	// If there is nothing left after trimming whitespace, the string is blank
+	if remark == "" {
+		return "Fine. Be that way!"
+	}
+
+	letterRegex, _ := regexp.Compile(`[a-zA-Z]`)
+
+	if remark == strings.ToUpper(remark) {
+		// An all uppercase question, note '42?' will land here
+		if remark[len(remark)-1:] == "?" {
+			// Check if there are letters in the question
+			if len(letterRegex.FindString(remark)) > 0 {
+				return "Calm down, I know what I'm doing!"
+			} else {
+				return "Sure."
+			}
+		}
+
+		// Check if there are letters or if it's e.g. '1,2,3'
+		if len(letterRegex.FindString(remark)) < 1 {
+			return "Whatever."
+		} else {
+			return "Whoa, chill out!"
+		}
+	}
+
+	if remark[len(remark)-1:] == "?" {
+		return "Sure."
+	}
+
+	return "Whatever."
+}
+
+func main() {
+	fmt.Print(Hey("KKBKB?"))
+}
